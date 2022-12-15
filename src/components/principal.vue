b5d6fd463d241d33728120143f0e701bc3daf56f
jpcamposgda <jpcamposgda@gmail.com>
2022-12-15 18:49:33 -0400

    responsive-quase-ok

--- src/components/principal.vue
+++ src/components/principal.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="q-my-md text-center  ">
+  <div class="q-my-md text-center lt-sm    "
+  style="margin-top: 50px"
+  >
     <q-img src="../assets/Programming-amico.svg" alt="dev" class="img"  />
     <div class="q-my-md" ><span class="nameP block ">João Pedro</span>
     <span class="corpoR">DESENVOLVEDOR FRONT-END</span>
@@ -10,28 +12,78 @@
         
       
   </div>
+<div class=" gt-xs fit row inline wrap justify-around items-center content-center"
+style="margin-top: 50px"
+>
+  <div 
+  style="
+  
+  
+  ">
+    <span class="nameP block" style="margin-left: 40px">
+      João Pedro
+    </span>
+    <span class="corpoR block">
+      DESENVOLVEDOR FRONT-END
+    </span>
+    <q-btn outline rounded class="inputRXL"  style="margin-left: 30px" label="BAIXAR CURRICULO" />
+  </div>
+  
+    <img src="../assets/Programming-amico.svg" alt="" 
+    style="
+    margin: 20px;
 
-  <div class=" text-center">
+    width: 400px;
+    height: 400px;
+    ">
+  
+</div>
+  
+  <div class=" text-center lt-md ">
   <div class="spacingP"><q-icon name="psychology" class="colorItem"  style="font-size: 24px" /><span class="skillP">HABILIDADES</span></div>
-    <div class="row listaR ">
+    <div class="row listaR fit  inline wrap justify-around items-center content-center">
       
-      <li class="col-2" ><span>JAVASCRIPT</span><img src="../assets/icons8-javascript-50.svg" alt=""><q-linear-progress :value="progressJ" dark  class="progressR" style="color: #FFD600" /></li>
-      <li class="col-2"><span>VUEJS</span><img src="../assets/icons8-vuejs-50.svg" alt=""><q-linear-progress :value="progressV"  dark  class="progressR" style="color: #3EAF7C" /></li>
-      <li class="col-2"><span>TYPESCRIPT</span><img src="../assets/icons8-typescript.svg" alt=""><q-linear-progress :value="progressT" dark  class="progressR" style="color: #1976D2" /></li>
-      <li class="col-2"><span>QUASAR</span><img src="../assets/quasar-logo-vertical.svg" class="quasarI" alt=""><q-linear-progress dark  :value="progressQ"  class="progressR" style="color: #00B4FF" /></li>
-      <li class="col-2"><span>NESTJS</span><img src="../assets/icons8-nestjs.svg" alt=""><q-linear-progress :value="progressN" dark   class="progressR" style="color: #F50057" /></li>
-      <li class="col-2"><span>FIREBASE</span><img src="../assets/icons8-firebase.svg" alt=""><q-linear-progress :value="progressF" dark   class="progressR" style="color: #FFC400" /></li>
+      <li style="width: 62px;" ><span>JAVASCRIPT</span><img style="width: 37px; height: 37px;" src="../assets/icons8-javascript-50.svg" alt=""><q-linear-progress :value="progressJ" dark  class="progressR" style="color: #FFD600" /></li>
+      <li style="width: 62px;"><span>VUEJS</span><img style="width: 37px; height: 37px;" src="../assets/icons8-vuejs-50.svg" alt=""><q-linear-progress :value="progressV"  dark  class="progressR" style="color: #3EAF7C" /></li>
+      <li style="width: 62px;"><span>TYPESCRIPT</span><img style="width: 37px; height: 37px;" src="../assets/icons8-typescript.svg" alt=""><q-linear-progress :value="progressT" dark  class="progressR" style="color: #1976D2" /></li>
+      <li style="width: 62px;"><span>QUASAR</span><img style="width: 37px; height: 37px;" src="../assets/quasar-logo-vertical.svg" class="quasarI" alt=""><q-linear-progress dark  :value="progressQ"  class="progressR" style="color: #00B4FF" /></li>
+      <li style="width: 62px;"><span>NESTJS</span><img style="width: 37px; height: 37px;" src="../assets/icons8-nestjs.svg" alt=""><q-linear-progress :value="progressN" dark   class="progressR" style="color: #F50057" /></li>
+      <li style="width: 62px;"><span>FIREBASE</span><img style="width: 37px; height: 37px;" src="../assets/icons8-firebase.svg" alt=""><q-linear-progress :value="progressF" dark   class="progressR" style="color: #FFC400" /></li>
     </div>
 
   </div>
+
+
+  <div class=" text-center gt-sm ">
+  <div class="spacingP"><q-icon name="psychology" class="colorItem"  style="font-size: 24px" /><span class="skillP">HABILIDADES</span></div>
+    <div class="row listaR fit  inline wrap justify-around items-center content-center">
+      
+      <li style="width: 62px;" ><span>JAVASCRIPT</span><img style="width: 50px; height: 50px;" src="../assets/icons8-javascript-50.svg" alt=""><q-linear-progress :value="progressJ" dark  class="progressR" style="color: #FFD600" /></li>
+      <li style="width: 62px;"><span>VUEJS</span><img style="width: 50px; height: 50px;" src="../assets/icons8-vuejs-50.svg" alt=""><q-linear-progress :value="progressV"  dark  class="progressR" style="color: #3EAF7C" /></li>
+      <li style="width: 62px;"><span>TYPESCRIPT</span><img style="width: 50px; height: 50px;" src="../assets/icons8-typescript.svg" alt=""><q-linear-progress :value="progressT" dark  class="progressR" style="color: #1976D2" /></li>
+      <li style="width: 62px;"><span>QUASAR</span><img style="width: 50px; height: 50px;" src="../assets/quasar-logo-vertical.svg" class="quasarI" alt=""><q-linear-progress dark  :value="progressQ"  class="progressR" style="color: #00B4FF" /></li>
+      <li style="width: 62px;"><span>NESTJS</span><img style="width: 50px; height: 50px;" src="../assets/icons8-nestjs.svg" alt=""><q-linear-progress :value="progressN" dark   class="progressR" style="color: #F50057" /></li>
+      <li style="width: 62px;"><span>FIREBASE</span><img style="width: 50px; height: 50px;" src="../assets/icons8-firebase.svg" alt=""><q-linear-progress :value="progressF" dark   class="progressR" style="color: #FFC400" /></li>
+    </div>
+
+  </div>
+
+
+
+  
 
   <div class=" text-center paddingR">
     
     <div class="spacingP"><q-icon name="info" class="colorItem"  style="font-size: 24px" /><span class="skillP">SOBRE</span></div>
     
-    <img src="../assets/jp.png" alt="">
+    <img src="../assets/jp.png" alt="" class="lt-sm" >
     
-    
+    <img src="../assets/jp.png" alt="" class="gt-xs"  style="
+    margin: 20px;
+
+    width: 250px;
+    height: 250px;
+    "> >
     <p style="
       font-family: 'Roboto Mono', 'Roboto;
       color: white;
@@ -88,6 +140,17 @@ export default {
   color: white;
 }
 
+
+.namePXL{
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400px;
+  font-size: 28px;
+  line-height: 50px;
+  
+  
+  color: white;
+}
+
 .skillP{
 
   font-family: 'Roboto Mono', monospace;;
@@ -102,7 +165,7 @@ export default {
 }
 .img {
 width: 230px;
-height: 230px;;
+height: 230px;; 
   
 }
 
@@ -115,9 +178,10 @@ height: 230px;;
   font-family: 'Roboto Mono', monospace;
   color: white;
   
-  font-size: 9px;
+  font-size: 11px;
   line-height: 15px;
   list-style: none;
+  
 }
 
 ul li {
@@ -132,6 +196,15 @@ ul li {
 
 }
 
+.corpoRXL{
+  font-family: 'Roboto Mono', monospace;
+  font-weight: 400px;
+  font-size: 18px;
+  line-height: 25px;
+  color: rgba(24, 114, 17, 1);  
+
+}
+
 .inputR{
 
   width: 181.83px;
@@ -139,6 +212,17 @@ ul li {
   color: #840AAF;
   font-family: 'Roboto Mono', monospace;
   margin-bottom: 128px;
+
+}
+
+.inputRXL{
+
+width: 181.83px;
+height: 35.94px;
+color: #840AAF;
+font-family: 'Roboto Mono', monospace;
+margin-top: 10px;
+
 
 }
 
@@ -156,8 +240,13 @@ ul li {
 }
 
 .progressR{
-  margin-left: 18px;
-  margin-top: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 5px;
   width: 32px;
+}
+
+.margin-lado{
+  margin-right: 200px;
 }
 </style>
