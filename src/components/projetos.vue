9aafff2d634b86cec30346ac32250462c133f945
jpcamposgda <jpcamposgda@gmail.com>
2023-03-08 18:55:12 -0400

    pizzaria

--- src/components/projetos.vue
+++ src/components/projetos.vue
@@ -1,224 +1,369 @@
 <template>
   <div class="about">
-
     <div class="text-center">
-      <h2 style="
-    font-family: 'Roboto Mono', monospace ;
-    font-weight: 400px;
-    font-size: 24px;
-    color: white;
-    ">
+      <h2
+        style="
+          font-family: 'Roboto Mono', monospace;
+          font-weight: 400px;
+          font-size: 24px;
+          color: white;
+        "
+      >
         PROJETOS
       </h2>
     </div>
-
-
-    <div class=" card cursor-pointer   ">
-
-      <a href="https://magnificent-klepon-051051.netlify.app">
-        <div class="card-child" >
-          <img src="../assets/feedbacker.png" alt="feedbacker" style="
-      object-fit: cover;
-      width: 100%;
-      border-top-left-radius: 10px ;
-      border-top-right-radius: 10px ;
-       " /> 
+    <div class="card-pizza cursor-pointer">
+      <a href="https://front-end-pizzaria.vercel.app/">
+        <div class="card-child">
+          <img
+            src="../assets/frontEnd.png"
+            alt="front"
+            style="
+              object-fit: cover;
+              width: 100%;
+              border-top-left-radius: 10px;
+              border-top-right-radius: 10px;
+            "
+          />
         </div>
-        
+
         <div class="my-card-down">
-          <span style="
-              font-family:  'Poppins', sans-serif ;
+          <span
+            style="
+              font-family: 'Poppins', sans-serif;
               color: white;
               font-weight: 400px;
               font-size: 24px;
-              margin-left: 20px">FeedBacker
+              margin-left: 20px;
+            "
+            >Front-End Pizzaria
           </span>
-          
-          <p style="
-              font-family:  'Poppins', sans-serif ;
+
+          <p
+            style="
+              font-family: 'Poppins', sans-serif;
               color: rgb(91, 91, 91);
               margin-left: 20px;
               font-size: 16px;
-              margin-top: 0px;">
-         Aplicação de feedBacks com vue3, TailwindCSS
-  <br /> para entrar basta digitar "jpcamposgda@hotmail.com" e a senha "1234"
+              margin-top: 0px;
+            "
+          >
+            Aplicação completa com back-end, feita com ReactJs e NodeJs.
+            
+            Essa aplicação simula uma pizzaria , tendo dois front-ends,
+            o primeiro é esse, sendo a parte da cozina onde recebe os pedidos, da outra interface que se chama App.
+            
           </p>
         </div>
+      </a>
+    </div>
+    <div class="card-pizza cursor-pointer">
+      <a href="https://app-pizzaria-three.vercel.app/">
+        <div class="card-child">
+          <img
+            src="../assets/App.png"
+            alt="App"
+            style="
+              object-fit: cover;
+              width: 100%;
+              border-top-left-radius: 10px;
+              border-top-right-radius: 10px;
+            "
+          />
+        </div>
 
+        <div class="my-card-down">
+          <span
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: white;
+              font-weight: 400px;
+              font-size: 24px;
+              margin-left: 20px;
+            "
+            >App-Pizzaria 
+          </span>
+
+          <p
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: rgb(91, 91, 91);
+              margin-left: 20px;
+              font-size: 16px;
+              margin-top: 0px;
+            "
+          >
+          Aplicação completa com back-end, feita com ReactJs e NodeJs.
+            
+            Essa é a segunda interface, simulando onde se registram os pedidos que vão para "cozinha" que é a parte de front-end.
+          </p>
+        </div>
       </a>
     </div>
 
-    <div class=" card cursor-pointer   ">
+    <div class="card cursor-pointer">
+      <a href="https://magnificent-klepon-051051.netlify.app">
+        <div class="card-child">
+          <img
+            src="../assets/feedbacker.png"
+            alt="feedbacker"
+            style="
+              object-fit: cover;
+              width: 100%;
+              border-top-left-radius: 10px;
+              border-top-right-radius: 10px;
+            "
+          />
+        </div>
 
-<a href="https://hahn-project-ok.vercel.app/">
-  <div class="card-child" >
-    <img src="../assets/índice.png" alt="feedbacker" style="
-object-fit: cover;
-width: 300px;
-height: 143.63px;
-border-top-left-radius: 10px ;
-border-top-right-radius: 10px ;
- " /> 
-  </div>
-  
-  <div class="my-card-down">
-    <span style="
-        font-family:  'Poppins', sans-serif ;
-        color: white;
-        font-weight: 400px;
-        font-size: 24px;
-        margin-left: 20px">Uniseg
-    </span>
-    
-    <p style="
-        font-family:  'Poppins', sans-serif ;
-        color: rgb(91, 91, 91);
-        margin-left: 20px;
-        font-size: 16px;
-        margin-top: 0px;">
-   Uma Aplicação criada com as técnologias Vuejs, Tailwind e Firebase. </p>
-  </div>
+        <div class="my-card-down">
+          <span
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: white;
+              font-weight: 400px;
+              font-size: 24px;
+              margin-left: 20px;
+            "
+            >FeedBacker
+          </span>
 
-</a>
-</div>
-
-<div class=" card cursor-pointer   ">
-
-<a href="https://alura-tracker-two-sigma.vercel.app/">
-  <div class="card-child" >
-    <img src="../assets/aluraTrack.png" alt="feedbacker" style="
-object-fit: cover;
-width: 300px;
-height: 143.63px;
-border-top-left-radius: 10px ;
-border-top-right-radius: 10px ;
- " /> 
-  </div>
-  
-  <div class="my-card-down">
-    <span style="
-        font-family:  'Poppins', sans-serif ;
-        color: white;
-        font-weight: 400px;
-        font-size: 24px;
-        margin-left: 20px">Alura Tracker
-    </span>
-    
-    <p style="
-        font-family:  'Poppins', sans-serif ;
-        color: rgb(91, 91, 91);
-        margin-left: 20px;
-        font-size: 16px;
-        margin-top: 0px;">
-  Aplicação feita com VUE 3 da Alura, foram usadas as técnologias vuejs, bulbaCSS, TypeScript. </p>
-  </div>
-
-</a>
-</div>
-
-<div class=" card cursor-pointer   ">
-
-<a href="https://abc-abc-theta.vercel.app/">
-  <div class="card-child" >
-    <img src="../assets/pinia.png" alt="feedbacker" style="
-object-fit: cover;
-width: 300px;
-height: 143.63px;
-border-top-left-radius: 10px ;
-border-top-right-radius: 10px ;
- " /> 
-  </div>
-  
-  <div class="my-card-down">
-    <span style="
-        font-family:  'Poppins', sans-serif ;
-        color: white;
-        font-weight: 400px;
-        font-size: 24px;
-        margin-left: 20px">Pinia
-    </span>
-    
-    <p style="
-        font-family:  'Poppins', sans-serif ;
-        color: rgb(91, 91, 91);
-        margin-left: 20px;
-        font-size: 16px;
-        margin-top: 0px;">
-  Um pequeno teste com Pinia store, para gerenciar estados globais na aplicação </p>
-  </div>
-
-</a>
-</div>
-
-<div class=" card cursor-pointer   ">
-
-<a href="https://jpcamposgda.github.io/final-code/">
-  <div class="card-child" >
-    <img src="../assets/devFinance.png" alt="feedbacker" style="
-object-fit: cover;
-width: 300px;
-height: 143.63px;
-border-top-left-radius: 10px ;
-border-top-right-radius: 10px ;
- " /> 
-  </div>
-  
-  <div class="my-card-down">
-    <span style="
-        font-family:  'Poppins', sans-serif ;
-        color: white;
-        font-weight: 400px;
-        font-size: 24px;
-        margin-left: 20px">DEV Finance
-    </span>
-    
-    <p style="
-        font-family:  'Poppins', sans-serif ;
-        color: rgb(91, 91, 91);
-        margin-left: 20px;
-        font-size: 16px;
-        margin-top: 0px;">
-   Aplicação de controle de gastos, com entrada e saída de valores. <br />
-            Feito com HTML, Javascript e CSS. os dados são guardados na localStorage
+          <p
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: rgb(91, 91, 91);
+              margin-left: 20px;
+              font-size: 16px;
+              margin-top: 0px;
+            "
+          >
+            Aplicação de feedBacks com vue3, TailwindCSS
+            <br />
+            para entrar basta digitar "jpcamposgda@hotmail.com" e a senha "1234"
           </p>
-  </div>
+        </div>
+      </a>
+    </div>
 
-</a>
-</div>
+    <div class="card cursor-pointer">
+      <a href="https://hahn-project-ok.vercel.app/">
+        <div class="card-child">
+          <img
+            src="../assets/índice.png"
+            alt="feedbacker"
+            style="
+              object-fit: cover;
+              width: 300px;
+              height: 143.63px;
+              border-top-left-radius: 10px;
+              border-top-right-radius: 10px;
+            "
+          />
+        </div>
 
-<div class=" card cursor-pointer   ">
+        <div class="my-card-down">
+          <span
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: white;
+              font-weight: 400px;
+              font-size: 24px;
+              margin-left: 20px;
+            "
+            >Uniseg
+          </span>
 
-<a href="https://jpcamposgda.github.io/FreeJimmy/">
-  <div class="card-child" >
-    <img src="../assets/freejimmy.png" alt="feedbacker" style="
-object-fit: cover;
-width: 300px;
-height: 143.63px;
-border-top-left-radius: 10px ;
-border-top-right-radius: 10px ;
- " /> 
-  </div>
-  
-  <div class="my-card-down">
-    <span style="
-        font-family:  'Poppins', sans-serif ;
-        color: white;
-        font-weight: 400px;
-        font-size: 24px;
-        margin-left: 20px">FreeLance
-    </span>
-    
-    <p style="
-        font-family:  'Poppins', sans-serif ;
-        color: rgb(91, 91, 91);
-        margin-left: 20px;
-        font-size: 16px;
-        margin-top: 0px;">
-   PageLanding responsiva, estática e com um background degrade</p>
-  </div>
+          <p
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: rgb(91, 91, 91);
+              margin-left: 20px;
+              font-size: 16px;
+              margin-top: 0px;
+            "
+          >
+            Uma Aplicação criada com as técnologias Vuejs, Tailwind e Firebase.
+          </p>
+        </div>
+      </a>
+    </div>
 
-</a>
-</div>
+    <div class="card cursor-pointer">
+      <a href="https://alura-tracker-two-sigma.vercel.app/">
+        <div class="card-child">
+          <img
+            src="../assets/aluraTrack.png"
+            alt="feedbacker"
+            style="
+              object-fit: cover;
+              width: 300px;
+              height: 143.63px;
+              border-top-left-radius: 10px;
+              border-top-right-radius: 10px;
+            "
+          />
+        </div>
+
+        <div class="my-card-down">
+          <span
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: white;
+              font-weight: 400px;
+              font-size: 24px;
+              margin-left: 20px;
+            "
+            >Alura Tracker
+          </span>
+
+          <p
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: rgb(91, 91, 91);
+              margin-left: 20px;
+              font-size: 16px;
+              margin-top: 0px;
+            "
+          >
+            Aplicação feita com VUE 3 da Alura, foram usadas as técnologias
+            vuejs, bulbaCSS, TypeScript.
+          </p>
+        </div>
+      </a>
+    </div>
+
+    <div class="card cursor-pointer">
+      <a href="https://abc-abc-theta.vercel.app/">
+        <div class="card-child">
+          <img
+            src="../assets/pinia.png"
+            alt="feedbacker"
+            style="
+              object-fit: cover;
+              width: 300px;
+              height: 143.63px;
+              border-top-left-radius: 10px;
+              border-top-right-radius: 10px;
+            "
+          />
+        </div>
+
+        <div class="my-card-down">
+          <span
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: white;
+              font-weight: 400px;
+              font-size: 24px;
+              margin-left: 20px;
+            "
+            >Pinia
+          </span>
+
+          <p
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: rgb(91, 91, 91);
+              margin-left: 20px;
+              font-size: 16px;
+              margin-top: 0px;
+            "
+          >
+            Um pequeno teste com Pinia store, para gerenciar estados globais na
+            aplicação
+          </p>
+        </div>
+      </a>
+    </div>
+
+    <div class="card cursor-pointer">
+      <a href="https://jpcamposgda.github.io/final-code/">
+        <div class="card-child">
+          <img
+            src="../assets/devFinance.png"
+            alt="feedbacker"
+            style="
+              object-fit: cover;
+              width: 300px;
+              height: 143.63px;
+              border-top-left-radius: 10px;
+              border-top-right-radius: 10px;
+            "
+          />
+        </div>
+
+        <div class="my-card-down">
+          <span
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: white;
+              font-weight: 400px;
+              font-size: 24px;
+              margin-left: 20px;
+            "
+            >DEV Finance
+          </span>
+
+          <p
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: rgb(91, 91, 91);
+              margin-left: 20px;
+              font-size: 16px;
+              margin-top: 0px;
+            "
+          >
+            Aplicação de controle de gastos, com entrada e saída de valores.
+            <br />
+            Feito com HTML, Javascript e CSS. os dados são guardados na
+            localStorage
+          </p>
+        </div>
+      </a>
+    </div>
+
+    <div class="card cursor-pointer">
+      <a href="https://jpcamposgda.github.io/FreeJimmy/">
+        <div class="card-child">
+          <img
+            src="../assets/freejimmy.png"
+            alt="feedbacker"
+            style="
+              object-fit: cover;
+              width: 300px;
+              height: 143.63px;
+              border-top-left-radius: 10px;
+              border-top-right-radius: 10px;
+            "
+          />
+        </div>
+
+        <div class="my-card-down">
+          <span
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: white;
+              font-weight: 400px;
+              font-size: 24px;
+              margin-left: 20px;
+            "
+            >FreeLance
+          </span>
+
+          <p
+            style="
+              font-family: 'Poppins', sans-serif;
+              color: rgb(91, 91, 91);
+              margin-left: 20px;
+              font-size: 16px;
+              margin-top: 0px;
+            "
+          >
+            PageLanding responsiva, estática e com um background degrade
+          </p>
+        </div>
+      </a>
+    </div>
 
     <!-- <div class=" card q-pa-md cursor-pointer   ">
       <a href="https://hahn-project-ok.vercel.app/">
@@ -399,14 +544,19 @@ border-top-right-radius: 10px ;
 
     </div>
  -->
-
-
-
-
   </div>
 </template>
 
 <style scoped>
+
+.card-pizza{
+  width: 300px;
+  height: 180px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 300px;
+  display: block;
+}
 .card {
   width: 300px;
   height: 180px;
@@ -415,10 +565,12 @@ border-top-right-radius: 10px ;
   margin-bottom: 200px;
   display: block;
 }
-.card-child{
+.card-child {
   width: 300px;
   height: 143.63px;
 }
+
+
 
 .my-card-down {
   border: 0;
@@ -427,15 +579,10 @@ border-top-right-radius: 10px ;
   border-bottom-right-radius: 10px;
   padding: 10px;
 
-
- 
   box-shadow: 0px 4px 100px rgba(0, 0, 0, 0.56);
 }
 
 a {
   text-decoration: none;
 }
-
-
-
 </style>
